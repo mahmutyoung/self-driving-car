1ee7929c5c779833fdcd38ecfa0f580761718b6f
mahmutyoung <mahmutyoung@gmail.com>
2023-06-28 23:38:07 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById('myCanvas');
+canvas.width = 200;
+const ctx = canvas.getContext('2d');
+
+canvas.height = window.innerHeight;
+console.log(canvas.width);
+console.log(canvas);
