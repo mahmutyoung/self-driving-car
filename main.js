648f12b86d3caf92c9d73ab96861606ec0bb8a74
mahmutyoung <mahmutyoung@gmail.com>
2023-06-29 18:16:09 +0300

    car feature added

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 const canvas = document.getElementById('myCanvas');
 canvas.width = 200;
+canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
 
-canvas.height = window.innerHeight;
 console.log(canvas.width);
 console.log(canvas);
+ctx.fillStyle = 'red';
+const car = new Car(100, 300, 30, 50);
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  car.draw(ctx);
+  console.log(car.speed);
+  car.update();
+  requestAnimationFrame(animate);
+}
+animate();
